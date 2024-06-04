7bd3d331146d75074191d67ae31a5e5cff8c87dd
tanyavsharma90 <121803412+tanyavsharma90@users.noreply.github.com>
2024-06-04 20:32:07 +0000

    function declartion in diffrent ways

--- Jsfun/function.js
+++ Jsfun/function.js
@@ -41,4 +41,79 @@ function handleArrya(getArray){
 
 // console.log(handleArrya(myArray));
 
-console.log(handleArrya(["t.v", "table","bat","boll"]))
+// console.log(handleArrya(["t.v", "table","bat","boll"]))
+
+//**********local scope and global scope in function and if else condition********************* */
+
+let a = "1";
+// const b = "2";
+// var c = "3";
+
+if(true){
+    let a = "3090";
+    const b = "2";
+     var c = "3";
+    //  console.log(`let a has block scope ${a}`)
+}
+
+// console.log(` And this let "a" has global scope  ${a}`)
+// console.log(b);
+// console.log(c);   //var variable has global and local scope both
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//nesting in function
+
+function fruits(){
+    const fruit = "mango";
+    
+    function chocolate(){
+        const chocoBar = "icream";
+        // console.log(fruit);       //out variable excess
+        // console.log(chocoBar)                           // child can excess parent,s variable
+
+    }
+
+    // console.log(chocoBar); //nesting varable excess in parent function
+                            // nesting variable has their own block scope only
+
+    chocolate();
+}
+
+fruits();
+
+
+//*********************same in if else global and local scope ***********************/
+
+if(true){
+    const user = "tannnu";
+
+    if(user === "tannnu"){
+        const website = "insta";
+        // console.log(user);    // only this will print becuse child can excess their parent,s variable
+    }
+
+    // console.log(website);   it has a block that,s why it will not excess
+}
+
+// console.log(user);
+
+
+//++++++++++++++++++++++++ so many ways you will be able to define the function+++++++++++++++++++
+
+// console.log(addOne(2));
+
+function addOne(num){
+    return num + 1;
+}
+
+// second way to hold function in a variable******************
+
+
+// console.log(addTwo(5))   //************ before decration funtion console will not print */
+
+const addTwo = function (num){
+    return num + 2;
+}
+
+// console.log(addTwo(5))    //******** after declartion code will excute */
+
